8bc001c9cd7c9214296a032958939f967f1db328
josepsmith <josepsmith@wayfair.com>
2016-08-15 15:14:37 -0400

    Swtiched to Angular 1.x.

--- app/app.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'not-stack-overflow',
-  template: '<h1>Dummy template</h1>'
-})
-export class AppComponent { }
---
+++ app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('not-stack-overflow', []);
+
+app.service('questionService', function($http) {
+	this.add = function(submission) {
+
+	}
+});
+
+app.controller('controller', function($scope, questionService) {
+
+});
